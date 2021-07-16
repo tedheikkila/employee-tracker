d6185059c0a6037c45833d21975d55d75d24c9f0
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-16 16:54:36 -0500

    created add/view roles fcns

--- database.js
+++ database.js
@@ -17,7 +17,8 @@ connection.connect((err) => {
 viewDept = (finishViewDept) => {
   console.log("Viewing all departments")
   connection.query("SELECT * FROM department", function (err, res) {
-    if (err) {console.log(err)
+    if (err) {
+      console.log(err)
     } else {
       console.table(res)
     }
@@ -25,7 +26,20 @@ viewDept = (finishViewDept) => {
   })
 }
 
-// addDept: creates a department and shows updated dept table
+// viewRoles: views all roles
+viewRoles = (finishViewRoles) => {
+  console.log("Viewing all roles")
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.table(res)
+    }
+    finishViewRoles(err, res)
+  })
+}
+
+// addDept: creates a dept and shows updated dept table
 addDept = (finishAddDept) => {
   console.log("Adding new department...")
   inquirer.prompt([
@@ -34,14 +48,50 @@ addDept = (finishAddDept) => {
       message: "Enter new department name:",
       name: "newDepartment"
     }
-  ]).then(function(response) {
+  ]).then(function(res) {
     connection.query("INSERT INTO department SET ?", 
+    // inserts user's res into department's name column
     {
-      name: response.newDepartment
+      name: res.newDepartment
     },
     function() {
-      console.log(`Deparment ${response.newDepartment} was created`)
+      console.log(`Deparment ${res.newDepartment} was created`)
       viewDept(finishAddDept)
+    }
+    )
+  }) 
+};
+
+// addRole: creates a role and shows updated role table
+addRole = (finishAddRole) => {
+  console.log("Adding new role...")
+  inquirer.prompt([
+    {
+      type:"input",
+      message: "Enter in new role's title:",
+      name: "newRole"
+    },
+    {
+      type:"input",
+      message: "Enter in their salary:",
+      name: "newSalary"
+    },
+    {
+      type:"number",
+      message: "Enter in their department ID:",
+      name: "deptID"
+    },
+  ]).then(function(res) {
+    // inserts user's res into role's columns (title, sal, deptID)
+    connection.query("INSERT INTO role SET ?", 
+    {
+      title: res.newRole,
+      salary: res.newSalary,
+      department_id: res.deptID
+    },
+    function() {
+      console.log(`Role ${res.newRole} was created`)
+      viewRoles(finishAddRole)
     }
     )
   }) 
@@ -60,5 +110,7 @@ module.exports = {
   "endEmployeeTracker": endEmployeeTracker,
   "addDept": addDept,
   "viewDept": viewDept,
+  "addRole": addRole,
+  "viewRoles": viewRoles,
 }
 
